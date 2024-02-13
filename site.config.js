50ba799025215c27486f7bb75f8afc082238dbc5
libalpm <159192189+libalpm64@users.noreply.github.com>
2024-02-12 20:45:14 -0500

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -32,7 +32,7 @@ const CONFIG = {
   link: "https://morethan-log.vercel.app",
   since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "https://wallpapercave.com/wp/wp2292683.jpg", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
